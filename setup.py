9eb6c00fbf3ad16c60df35be14c2a2490c33e41f
Nikos Tsakiridis <tsakirin@auth.gr>
2021-11-11 11:23:50 +0200

    :bookmark: bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, "README.md"), encoding="utf-8") as f:
 # This call to setup() does all the work
 setup(
     name="spectraxai",
-    version="0.1.2",
+    version="0.2.0",
     packages=find_packages(include=["spectraxai", "spectraxai.utils"]),
     python_requires=">=3.6",
     install_requires=[
