b62055f2f0e73df66c0bc73f72bea525e5689f34
Nikos Tsakiridis <tsakirin@auth.gr>
2022-05-03 12:51:05 +0300

    :white_check_mark: update example to run on azure devops

--- examples/example1.py
+++ examples/example1.py
@@ -1,9 +1,16 @@
-from spectraxai.dataset import DatasetSplit
+import os
+
+import pandas
+
+from spectraxai.dataset import Dataset, DatasetSplit
 from spectraxai.spectra import SpectralPreprocessing
 from spectraxai.models import Model, StandardModel
-from spectraxai.utils.datasets import load_GR_SSL
 
-dataset = load_GR_SSL()
+
+DATA_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data"))
+
+df = pandas.read_csv(os.path.join(DATA_FOLDER, "SSL_GR.csv"))
+dataset = Dataset(df.loc[:, "350":"2500":20], df.OM)
 idx_trn, idx_tst = dataset.train_test_split(DatasetSplit.CROSS_VALIDATION, 5)
 
 methods = [
@@ -35,6 +42,6 @@ for method in methods:
         res = StandardModel(Model.PLS).train_and_test(dataset, method, fold)
         print(
             "{0}, fold {1}: RMSE {2:.2f}".format(
-                res[0]["pre-process"], i + 1, res[0]["RMSE"]
+                res[0]["pre_process"], i + 1, res[0]["RMSE"]
             )
         )
