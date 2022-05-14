b486613b17fb5ec7fdd947e05946ecc5e3cd69b0
Nikos Tsakiridis <tsakirin@auth.gr>
2022-05-14 18:48:05 +0300

    :bug: pass resources path as path and not generator

--- spectraxai/utils/datasets.py
+++ spectraxai/utils/datasets.py
@@ -8,10 +8,11 @@ DATA_FOLDER = "spectraxai.data"
 
 
 def load_GR_SSL(subsampling=20, properties=["OM"]):
-    df = pandas.read_csv(resources.path(DATA_FOLDER, "SSL_GR.csv"))
-    return Dataset(
-        X=df.loc[:, "350":"2500":subsampling],
-        Y=df.loc[:, properties],
-        X_names=range(350, 2501, subsampling),
-        Y_names=properties,
-    )
+    with resources.path(DATA_FOLDER, "SSL_GR.csv") as p:
+        df = pandas.read_csv(p)
+        return Dataset(
+            X=df.loc[:, "350":"2500":subsampling],
+            Y=df.loc[:, properties],
+            X_names=range(350, 2501, subsampling),
+            Y_names=properties,
+        )
